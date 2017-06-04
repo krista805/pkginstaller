a6f37c5462cfcadb66bf87ffaf61ca04a2304938
Krista Simmons <kristasimmons.design@gmail.com>
2017-06-04 14:54:09 -0700

    added additional test to make sure its setup correctly

--- test/pkginstallerTest.js
+++ test/pkginstallerTest.js
@@ -1,9 +1,15 @@
+var mocha = require('mocha')
+var expect = require('chai').expect
 // bring in assertion library
 const assert = require('chai').assert;
 // bring in js files
 const pkginstaller = require('../pkginstaller');
 
+
 describe('Pkginstaller', function(){
+  it("should run tests", function(){
+    expect(true).to.be.true
+  });
   it('pkginstaller should return hello', function(){
     assert.equal(pkginstaller(), 'hello');
   });
